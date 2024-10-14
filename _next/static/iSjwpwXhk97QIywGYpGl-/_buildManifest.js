d1ca941e0ae8b1109fd4ad2da7c3e5f5f86c175f
RIvance <rivance.0@gmail.com>
2024-10-14 21:29:01 +0800

    0

---
+++ _next/static/iSjwpwXhk97QIywGYpGl-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5e123bffa7f93b35.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
